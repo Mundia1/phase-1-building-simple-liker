995cb17882651060832ba21f212588c561fc7bd1
Mundia1 <waithaka.mundia@gmail.com>
2025-03-29 11:42:59 +0300

    feat: add simple liker with multiple hearts and error modal

--- main.js
+++ main.js
@@ -3,9 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll('.like-glyph');
+const errorModal = document.querySelector('#modal');
 
+// Add hidden class to modal initially (could also be done in HTML)
+errorModal.classList.add('hidden');
 
-
+// Add event listener to each heart
+hearts.forEach(heart => {
+    heart.addEventListener('click', () => {
+        if (!heart.classList.contains('activated-heart')) {
+            mimicServerCall()
+                .then(() => {
+                    heart.innerHTML = FULL_HEART;
+                    heart.classList.add('activated-heart');
+                })
+                .catch((error) => {
+                    errorModal.classList.remove('hidden');
+                    document.querySelector('#modal-message').textContent = error;
+                    setTimeout(() => {
+                        errorModal.classList.add('hidden');
+                    }, 3000);
+                });
+        } else {
+            heart.innerHTML = EMPTY_HEART;
+            heart.classList.remove('activated-heart');
+        }
+    });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
